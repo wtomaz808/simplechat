3a68c8137bfe722310a5d5d42d540a802341cc51
Paul Lizer <paullizer@microsoft.com>
2025-05-22 12:04:21 -0400

    removing deployment, updated IAC coming soon

--- application/single_app/config.py
+++ application/single_app/config.py
@@ -86,7 +86,7 @@ executor.init_app(app)
 
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['SESSION_TYPE'] = 'filesystem'
-app.config['VERSION'] = '0.213.001'
+app.config['VERSION'] = '0.213.002'
 Session(app)
 
 CLIENTS = {}
